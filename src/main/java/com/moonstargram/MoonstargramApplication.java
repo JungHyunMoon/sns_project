791b6a350010d451df09b90fd0f55129b98f527f
DESKTOP-JPP5GVA\Moon <foqlzm12345@gmail.com>
2023-01-17 02:10:00 +0900

    초기 환경 세팅

---
+++ src/main/java/com/moonstargram/MoonstargramApplication.java
@@ -0,0 +1,13 @@
+package com.moonstargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonstargramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonstargramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moonstargram/MoonstargramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moonstargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonstargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
